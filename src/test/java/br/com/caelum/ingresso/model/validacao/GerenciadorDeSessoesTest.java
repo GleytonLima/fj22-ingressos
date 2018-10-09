475b268c5b488bbd68180b3b5817769306e09d52
Gleyton <gleytonclima@gmail.com>
2018-10-09 15:18:29 -0400

    Incluido novos casos de teste

--- src/test/java/br/com/caelum/ingresso/model/validacao/GerenciadorDeSessoesTest.java
+++ src/test/java/br/com/caelum/ingresso/model/validacao/GerenciadorDeSessoesTest.java
@@ -2,6 +2,7 @@ package br.com.caelum.ingresso.model.validacao;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,27 +16,64 @@ import br.com.caelum.ingresso.model.Sessao;
 
 public class GerenciadorDeSessoesTest {
 	
+	private GerenciadorDeSessoes gs;
 	private Sessao sessaoNova;
+	
+	private Filme filme;
+	private Sala sala;
+	
+	private Sessao sessaoDasDez;
+	private Sessao sessaoDasTreze;
+	private Sessao sessaoDasDezoito;
 	
 	@Before
 	public void preparaCenario() {
+		this.gs = new GerenciadorDeSessoes();
 		LocalTime horario = LocalTime.of(10,00);
-		Filme filme = new Filme("Matrix", Duration.ofMinutes(120), "Ação");
-		Sala sala = new Sala("Sala 01");
-		sessaoNova = new Sessao(horario, filme, sala);
+		this.filme = new Filme("Matrix", Duration.ofMinutes(120), "Ação");
+		this.sala = new Sala("Sala 01");
+		this.sessaoNova = new Sessao(horario, filme, sala);
+		
+		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), filme, sala);
+		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"), filme, sala);
 	}
 	
 	@Test
 	public void deveCaberSessaoSeListaEstiverVazia() {
 		//Cria cenario
 		List<Sessao> sessoes = Collections.emptyList();
-		//Simulacao
-		GerenciadorDeSessoes gs = new GerenciadorDeSessoes();		
-		
-		boolean coube = gs.cabe(sessaoNova, sessoes);
+		//Simulacao		
+		boolean coube = this.gs.cabe(sessaoNova, sessoes);
 		
 		//Assercao
 		Assert.assertTrue(coube);
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessaoNoMesmoHorario() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		Assert.assertFalse(this.gs.cabe(sessaoDasDez, sessoes));
+		
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessoesTerminandoDentroDoHorarioDeUmaSessaoJaExistente() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		Sessao sessaoQueTerminaDentroDeUmaOutra = new Sessao(sessaoDasDez.getHorario().minusHours(1), filme, sala);
+		Assert.assertFalse(this.gs.cabe(sessaoQueTerminaDentroDeUmaOutra, sessoes));
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessoesIniciandoDentroDoHorarioDeUmaSessaoJaExistente() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		Sessao sessaoQueComecaDentroDeUmaOutra = new Sessao(sessaoDasDez.getHorario().plusHours(1), filme, sala);
+		Assert.assertFalse(this.gs.cabe(sessaoQueComecaDentroDeUmaOutra, sessoes));
+	}
+	@Test
+	public void garanteQueDevePermitirUmaInsercaoEntreDoisFilmes() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez, sessaoDasDezoito);
+		Assert.assertTrue(this.gs.cabe(sessaoDasTreze, sessoes));
 	}
 
 }
