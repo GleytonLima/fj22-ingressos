438740ed1207f11a846362d45f1e2dfc0c1ba715
Gleyton <gleytonclima@gmail.com>
2018-10-10 09:29:48 -0400

    Incluindo caso de teste calculo de valor da Sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -10,37 +10,37 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Sessao {
-	
+
 	@Id
 	@GeneratedValue
 	private Integer id;
 
 	private LocalTime horario;
-	
+
 	@ManyToOne
 	private Filme filme;
-	
+
 	@ManyToOne
-	private Sala sala;	
-	
-	
+	private Sala sala;
+
 	public Sessao() {
-		
+
 	}
-	
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public LocalTime getHorario() {
 		return horario;
 	}
@@ -52,23 +52,25 @@ public class Sessao {
 	public Filme getFilme() {
 		return filme;
 	}
+
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
+
 	public Sala getSala() {
 		return sala;
 	}
+
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
-	
+
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
-	
+
 	public BigDecimal getPreco() {
 		return sala.getPreco().add(filme.getPreco());
 	}
-	
 
 }
